cb4e7c8ea213f71930382664aecb4fa9002997df
Valery Kuzivanov <valery.kuzivanov@sibers.com>
2016-09-27 16:10:44 +0600

    model orm file

---
+++ src/AppBundle/Services/Db.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Lib;
+
+use AppBundle\Controller\DbController;
+
+class DbLib
+{
+//    protected $db;
+//
+//    public function __construct( DbController $db)
+//    {
+//        $this->db = $db;
+//    }
+//
+//    public function createAction( )
+//    {
+//
+//    }
+
+
+}
